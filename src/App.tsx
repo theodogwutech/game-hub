d3ade23db6129b849bc9df0e3e6a12a02a164c44
Valentine Offiah <voffiah@gmail.com>
2023-08-12 15:57:47 +0100

    Build the colorMode switch

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,10 @@ function App() {
       </GridItem>
 
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
 
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
